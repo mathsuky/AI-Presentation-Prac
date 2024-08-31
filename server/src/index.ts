6e136cfd117bc3ab11aa02eb19eef5088d2c09db
taka231 <taka23131@gmail.com>
2024-08-31 11:53:19 +0900

    feature: add server

---
+++ server/src/index.ts
@@ -0,0 +1,37 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { ChatOpenAI } from "@langchain/openai";
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
+
+require("dotenv").config();
+
+const chatModel = new ChatOpenAI();
+
+const app = new Hono();
+
+const schema = z.object({
+  message: z.string(),
+});
+
+app.post(
+  "/chat",
+  zValidator("json", schema, (result, c) => {
+    if (!result.success) {
+      return c.json({ error: "invalid json" }, 400);
+    }
+  }),
+  async (c) => {
+    const { message } = c.req.valid("json");
+    const aiMessageChunk = await chatModel.invoke(message);
+    return c.json({ content: aiMessageChunk });
+  },
+);
+
+const port = Number(process.env.PORT) || 3000;
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
