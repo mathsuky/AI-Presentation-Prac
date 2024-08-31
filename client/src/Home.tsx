d9d71b4ec33fbb7830887eea014cf21add81de8c
masky5859 <mathsuky2406@gmail.com>
2024-08-31 12:36:25 +0900

    add: UIライブラリを追加した

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <Button>Click me!</Button>
+    </>
+  );
 };
 
 export default Home;
