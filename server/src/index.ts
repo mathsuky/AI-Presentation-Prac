2edf6f035beeef620110d3c88ad1d9e361c4fdbc
taka231 <taka23131@gmail.com>
2024-08-31 17:26:17 +0900

    feature: /chatに、スライド毎に分割された文字起こしを渡すことが出来るようにした

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 const schema = z.object({
-  message: z.string(),
+  message: z.union([z.string(), z.string().array()]),
   images: z.string().array().optional(),
 });
 
@@ -52,11 +52,18 @@ app.post("/audio-to-text", async (c) => {
   return c.json({ text: transcription.text });
 });
 
-const orderText = `あなたはプレゼンテーションの授業の先生です。生徒のプレゼンテーションの音声の文字起こしが文字列として、スライドが画像の列として順番に渡されるので、生徒に適切なフィードバックを与えてください。日本語で答えてください。以下の3つの事柄についてフィードバックしてください。
+const orderText = (is_single_text: boolean) => {
+  const single_text_structure =
+    "生徒のプレゼンテーションの音声の文字起こしが文字列として、スライドが画像の列として順番に渡されるので";
+  const multi_text_structure =
+    "生徒のプレゼンテーションのスライドの画像と、そのスライドに対応する発表の音声の文字起こしが交互に渡されるので";
+  const text = `あなたはプレゼンテーションの授業の先生です。${is_single_text ? single_text_structure : multi_text_structure}、生徒に適切なフィードバックを与えてください。日本語で答えてください。文字起こしとスライドから得られる情報のみから答えてください。以下の3つの事柄についてフィードバックしてください。
 contradiction: 文字起こしとスライドとの間に食い違いがある場合、その食い違いを列挙してください。無い場合は空の配列を返してください。
-potential questions: 生徒のプレゼンテーションに対して、想定される質問を2つ以上列挙してください。
-improvement: 生徒のスライドや、発表の仕方に対して、改善点を2つ以上列挙してください。
+potential questions: 生徒のプレゼンテーションに対して、想定される質問を沢山挙げてください。
+improvement: 生徒のスライドや、発表の仕方に対して、改善点を沢山挙げてください。
 `;
+  return text;
+};
 
 const feedbackSchema = z.object({
   contradiction: z.string().array(),
@@ -73,16 +80,39 @@ app.post(
   }),
   async (c) => {
     const { message, images } = c.req.valid("json");
-    let content: MessageType[] = [{ type: "text", text: message }];
-    if (images) {
-      for (const image of images) {
-        content.push({ type: "image_url", image_url: { url: image } });
+    let content: MessageType[] = [];
+    const is_single_text = typeof message === "string";
+    if (is_single_text) {
+      content = [{ type: "text", text: message }];
+      if (images) {
+        for (const image of images) {
+          content.push({ type: "image_url", image_url: { url: image } });
+        }
+      }
+    } else {
+      if (!images) {
+        return c.json({ error: "images is required for multiple text" }, 400);
+      }
+      if (message.length !== images.length) {
+        return c.json(
+          { error: "message and images must have the same length" },
+          400,
+        );
+      }
+      for (let i = 0; i < message.length; i++) {
+        content.push({ type: "image_url", image_url: { url: images[i] } });
+        content.push({ type: "text", text: message[i] });
       }
     }
     const aiMessageChunk = await chatModel
       .withStructuredOutput(feedbackSchema)
       .invoke([
-        { role: "system", content: [{ type: "text", text: orderText }] },
+        {
+          role: "system",
+          content: [
+            { type: "text", text: orderText(is_single_text) },
+          ],
+        },
         {
           role: "user",
           content,
